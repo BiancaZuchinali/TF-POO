f2031f4d96eeb361242a153a489b0e942f9a729a
Bianca Zuchinali <107509260+BiancaZuchinali@users.noreply.github.com>
2023-05-31 15:45:44 -0300

    Create EmpresaService.h

---
+++ EmpresaService.h
@@ -0,0 +1,17 @@
+#ifndef EMPRESASERVICE_HPP
+#define EMPRESASERVICE_HPP
+
+#pragma once
+
+class EmpresaService
+{
+public:
+    EmpresaService();
+    ~EmpresaService();
+
+    Empresa* get();
+private:
+
+};
+
+#endif
